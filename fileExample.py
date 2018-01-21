816bb78a35e00f501a5e9c2f8173b5a9e83c8001
Ayush456 <guptaayush7232@gmail.com>
2018-01-22 00:25:23 +0530

    second commit

--- fileExample.py
+++ fileExample.py
@@ -9,3 +9,10 @@ print("Mode of operation is : ",filename.mode)
 length= len(filename.readline())
 print("Filesize : ", length )
 
+str = "this is ayush gupta"
+str2 =str.split(" ")
+
+for contents in str2:
+    print("\n",contents)
+
+
