f48d429478e43d71aae0b7d19651e6f062823639
Ayush456 <guptaayush7232@gmail.com>
2018-01-22 01:16:15 +0530

    tell in file

--- fileExample.py
+++ fileExample.py
@@ -7,19 +7,17 @@ print("Mode of operation is : ",filename.mode)
 #for index in range(len(filename)):
  #    line =
 count = 0
+start=filename.tell()
+print("Start of file : ",start)
 for index in filename :
     #line = next(filename)
     count = count + 1
+
 str = "#End of the file"
 print("The no of lines is : ",count)
-#print(filename.tell())
+print("EOF : ",filename.tell())
 
-filename.seek(0,2)
-filename.write("\n")
-filename.write(str)
 
-filename.seek(0,0)
-for index in filename :
-    print(index)
+
 
 
