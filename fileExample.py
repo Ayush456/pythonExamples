6b95eb58d02fd7a18105503c4a1ae00c5e3ef4ec
Ayush456 <guptaayush7232@gmail.com>
2018-01-21 23:52:45 +0530

    initial

--- fileExample.py
+++ fileExample.py
@@ -3,3 +3,9 @@
 filename = open("support.py","r+")
 print("Name of the file is : ",filename.name)
 print("Mode of operation is : ",filename.mode)
+
+#for index in range(len(filename)):
+ #    line =
+length= len(filename.readline())
+print("Filesize : ", length )
+
