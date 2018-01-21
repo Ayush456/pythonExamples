5f7d5d1eba0221170167c5c3a917cb0077a8b507
Ayush456 <guptaayush7232@gmail.com>
2018-01-21 22:43:06 +0530

    2nd file

---
+++ pythonDemo.py
@@ -0,0 +1,15 @@
+def ayush( str ):
+    str2=str+"hello"
+    return str2
+def gupta( str ):
+   str3=str+"helloG"
+   return str3
+
+print("Enter a value : ",end="\n")
+strInput = input()
+getopt=ayush(strInput)
+print(getopt)
+getopt2=gupta(strInput)
+
+print(getopt2)
+
