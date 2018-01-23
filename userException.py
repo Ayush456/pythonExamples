caa534f11a3f9e42c56676c8d91322900959cc4f
Ayush456 <guptaayush7232@gmail.com>
2018-01-23 21:16:18 +0530

    User defined exception example

---
+++ userException.py
@@ -0,0 +1,13 @@
+
+class UserException( Exception ):
+    def __init__(self,var):
+        self.var = var
+
+try:
+    value = int(input("enter a number : "))
+    if value < 10 :
+        raise UserException("this is my exception")
+except UserException as e:
+    print("Throwing an Exception",e.var)
+
+#
